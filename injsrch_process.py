2cb55dbbf30659ed76e879fbfdeb40712f928f91
maxisi <isi.maxi@gmail.com>
2014-07-26 19:02:28 -0700

    idem

--- injsrch_process.py
+++ injsrch_process.py
@@ -97,7 +97,7 @@ for m in g.search_methods:
         # define covariance matrix
         cov = np.dot(np.dot(V.T, W**2), V) # See 'Covariance' page in Polarizations tab of LIGO 2013 Notebook
         
-        VtW = V.T.dot(W)
+        VtW = np.dot(V.T, W)
         Utb = np.dot(U.T, b)
         
         # results:
