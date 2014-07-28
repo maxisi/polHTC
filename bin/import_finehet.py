ba5b57b4ce678ddc36b8c417a6db7c85c3e846d5
maxisi <isi.maxi@gmail.com>
2014-07-28 11:00:02 -0700

    import corrections 2
    added try to import psrs

--- bin/import_finehet.py
+++ bin/import_finehet.py
@@ -31,6 +31,8 @@ def imp(det, run, origin_path, destination_path):
     Imports det, run data for all pulsars in origin_path to destination_path (hdf5)
     '''
     
+    print('Importing: ')
+    
     pre = 'finehet_'
     pst = '_' + det
     
@@ -42,40 +44,47 @@ def imp(det, run, origin_path, destination_path):
     
     # obtain names of PSRs in directory
     allpsrs = [f.strip(pre).strip(pst) for f in fnames]
+    imported = []
     
     # open each finehet file and import to hdf5
     for psr in allpsrs:
-        print psr #
+        
         filename = pre + psr + pst
         
-        with open(origin_path + filename, 'r') as f_txt:
+        try:
+            with open(origin_path + filename, 'r') as f_txt:
             
-            # extract data from file
-            t = []
-            re = []
-            im = []
+                # extract data from file
+                t = []
+                re = []
+                im = []
             
-            for row in f_txt.readlines():
-                rowlist = row.split('\t')
-                t += [float(rowlist[0])]
-                re += [float(rowlist[1])]
-                im += [float(rowlist[2])]
+                for row in f_txt.readlines():
+                    rowlist = row.split('\t')
+                    t += [float(rowlist[0])]
+                    re += [float(rowlist[1])]
+                    im += [float(rowlist[2])]
             
-            d = np.array(re) + 1j*np.array(im, dtype=np.float64)
+                d = np.array(re) + 1j*np.array(im, dtype=np.float64)
                         
-            # create hdf5 file
-            try:
-                f_hdf5 = h5py.File(destination_path + filename + '.hdf5', 'w')
+                # create hdf5 file
+                try:
+                    f_hdf5 = h5py.File(destination_path + filename + '.hdf5', 'w')
 
-                f_hdf5.create_dataset('time', data=np.array(t, dtype=np.int))
-                f_hdf5.create_dataset('data', data=d, dtype=np.complex_)
-                
-                print det + run + 'data for' + str(len(allpsrs)) + ' PSRs imported to: ' + destination_path
-                
-            except:
-                print 'Could not write finehet to HDF5:'
-                print sys.exc_info()[0]
-                sys.exit()
+                    f_hdf5.create_dataset('time', data=np.array(t, dtype=np.int))
+                    f_hdf5.create_dataset('data', data=d, dtype=np.complex_)
+                    
+                    imported += [psr] # mark as successfully imported
+                    print psr + ', '
+                    
+                except IOError:
+                    print 'Could not write finehet to HDF5:'
+                    print sys.exc_info()[0]
+                    sys.exit()
+        except:
+            print '\nCould not import: ' + psr
+    
+        print '\n' + det + ' ' + run + ' data for' + str(len(imported)) + ' PSRs imported to: ' + destination_path
 
 
 if run == 'S5':
