eb757d562f456a7b6d20d02eabdc7c53b9aa9bde
maxisi <isi.maxi@gmail.com>
2014-08-06 17:08:18 -0700

    clean vectors

--- bin/clean.sh
+++ bin/clean.sh
@@ -4,5 +4,8 @@ rm -r analyses/*
 rm subs/*
 rm logs/*
 rm /usr1/max.isi/*
+rm globals/vectors/*
+rm /atlas/user/atlas3/max.isi/*
+rm /atlas/user/atlas1/max.isi/*
 
 echo "Deleted all analysis files."
