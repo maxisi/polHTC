43280efbb34b0f598f939b95dea442e465a00739
maxisi <misi@ligo.caltech.edu>
2014-10-06 09:39:13 -0700

    minor plot changes

--- scripts/paperplots.py
+++ scripts/paperplots.py
@@ -17,35 +17,35 @@ p = '/home/misi/results/'
 
 ###############################################################################
 # METHODS PLOTS (Crab)
-crab_gr = r.Results('H1', 'S5', 'J0534+2200', 'GR')
-crab_gr.load(path=p)
-# s vs hrec
-crab_gr.plot_hs(title=False)
-# p--value
-crab_gr.plot_p('s', title=False, star=10, starsize=12,
-            xlim=(0, 12), legend=False, methods=['G4v'])
-# s/hrec vs hinj example
-crab_gr.plot('s', methods=['GR'], title=False, legend=False, suffix='_ex')
-crab_gr.plot('h', methods=['GR'], title=False, legend=False, suffix='_ex',
-             ylim=(0, .9e-24))
+# crab_gr = r.Results('H1', 'S5', 'J0534+2200', 'GR')
+# crab_gr.load(path=p)
+# # s vs hrec
+# crab_gr.plot_hs(title=False)
+# # p--value
+# crab_gr.plot_p('s', title=False, star=10, starsize=12,
+#             xlim=(0, 12), legend=False, methods=['G4v'])
+# # s/hrec vs hinj example
+# crab_gr.plot('s', methods=['GR'], title=False, legend=False, suffix='_ex')
+# crab_gr.plot('h', methods=['GR'], title=False, legend=False, suffix='_ex',
+#              ylim=(0, .9e-24))
 
 ###############################################################################
 # CRAB RESULTS PLOTS
 
 # GR
-crab_gr.plot('s', methods=['GR', 'Sid'], band_conf=0, title=False,
-             suffix='_sid')
-crab_gr.plot('s', methods=['GR', 'G4v'], band_conf=0, title=False,
-             suffix='_g4v')
-# G4v
-crab_g4v = r.Results('H1', 'S5', 'J0534+2200', 'G4v')
-crab_g4v.load(path=p)
-
-crab_g4v.plot('s', methods=['GR', 'Sid'], band_conf=0, title=False,
-             suffix='_sid')
-crab_g4v.plot('s', methods=['GR', 'G4v'], band_conf=0, title=False,
-             suffix='_gr')
-
+# crab_gr.plot('s', methods=['GR', 'Sid'], band_conf=0, title=False,
+#              suffix='_sid')
+# crab_gr.plot('s', methods=['GR', 'G4v'], band_conf=0, title=False,
+#              suffix='_g4v')
+# # G4v
+# crab_g4v = r.Results('H1', 'S5', 'J0534+2200', 'G4v')
+# crab_g4v.load(path=p)
+#
+# crab_g4v.plot('s', methods=['GR', 'Sid'], band_conf=0, title=False,
+#              suffix='_sid')
+# crab_g4v.plot('s', methods=['GR', 'G4v'], band_conf=0, title=False,
+#              suffix='_gr')
+#
 ###############################################################################
 # ALL PSRs RESULTS PLOTS
 detectors = ['H1']#, 'L1', 'H2']
@@ -56,4 +56,4 @@ for d in detectors:
     for run in runs:
         for injkind in injections:
             results = r.ResultsMP(injkind, det=d, run=run, path=p)
-            results.plot_ref(filetype='png', det_conf=0)
+            results.plot_ref(filetype='png')
